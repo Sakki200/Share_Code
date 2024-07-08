603554b1e998265cf61475d7e66e34aaf4dee594
Salman Syed <syedsalman8986@gmail.com>
2024-07-08 12:53:34 +0200

    recup_code_EPURATION

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const codeTextarea = document.getElementById('code');
+
+  // Restore textarea content on page load
+  restoreTextareaContent();
+
+  // Save textarea content as user types
+  codeTextarea.addEventListener('input', () => {
+    localStorage.setItem('URL', codeTextarea.value);
+  });
+
+  function restoreTextareaContent() {
+    const savedCode = localStorage.getItem('currentCode');
+    if (savedCode) {
+      codeTextarea.value = savedCode;
+    }
+  }
+});
